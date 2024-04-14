5f6dfc1d13b814c6466b972feff7983fa90133db
liangtam <tammieliang@hotmail.com>
2024-04-13 23:26:24 -0700

    backend setup

---
+++ src/test/java/com/planningpoker/PlanningpokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planningpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningpokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
